869aaa69f29e144056765982207e9a1ea9ed837c
Salva Imm <mehdi73ee@gmail.com>
2024-06-25 17:09:52 +0330

    arrow for faq

--- src/collapsible.tsx
+++ src/collapsible.tsx
@@ -2,27 +2,28 @@ import { useState, ReactNode } from "react";
 import "./index.css";
 
 interface CollapsibleProps {
-    title: string;
-    children: ReactNode;
-  }
+  title: string;
+  children: ReactNode;
+}
 
 const Collapsible = ({ title, children }: CollapsibleProps) => {
-    const [isOpen, setIsOpen] = useState(false);
-  
-    const handleClick = () => {
-      setIsOpen(!isOpen);
-    };
-  
-    return (
-      <div className="collapsible">
-        <button className="collapsible-header" onClick={handleClick}>
-          {title}
-        </button>
-        <div className={`collapsible-content ${isOpen ? "open" : ""}`}>
-          {children}
-        </div>
-      </div>
-    );
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleClick = () => {
+    setIsOpen(!isOpen);
   };
 
-  export default Collapsible;
+  return (
+    <div className="collapsible">
+      <button className="collapsible-header" onClick={handleClick}>
+        <span>{title}</span>
+        <div className="arrow">{isOpen ? <img className="arrow" src="/arrow-26.svg" alt="" /> : <img className="arrow" src="/arrow-25.svg" alt="" />}</div>
+      </button>
+      <div className={`collapsible-content ${isOpen ? "open" : ""}`}>
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default Collapsible;
